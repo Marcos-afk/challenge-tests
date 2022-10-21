feedccb5ad881b1996e9a7ea629571b59fa535aa
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-10-21 08:08:24 -0300

    implementation of user profile lookup integration tests

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.test.ts
@@ -0,0 +1,46 @@
+import { AppSource, InitializeConnection } from "../../../../database";
+import request from "supertest";
+import { app } from "../../../../app";
+import { ICreateUserDTO } from "../createUser/ICreateUserDTO";
+
+describe("Create user controller", () => {
+  beforeAll(async () => {
+    await InitializeConnection();
+    await AppSource.runMigrations();
+  });
+
+  afterAll(async () => {
+    await AppSource.dropDatabase();
+    await AppSource.destroy();
+  });
+
+  it("Should be able to return a user", async () => {
+    const user: ICreateUserDTO = {
+      name: "user test",
+      email: "user@email.test",
+      password: "12345678",
+    };
+
+    await request(app)
+      .post("/api/v1/users")
+      .send({
+        ...user,
+      });
+
+    const responseToken = await request(app)
+      .post("/api/v1/sessions")
+      .send({
+        ...user,
+      });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .get("/api/v1/profile")
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(200);
+  });
+});
